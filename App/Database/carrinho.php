192ecc69b5525e13fba5827933b3cdff4a3da0fa
Fabrício Paixão <ftptiago@users.noreply.github.com>
2020-04-15 23:01:02 -0300

    Remoção e atualização
    Efetuada a atualização da função count para atender as mudanças do PHP nas versões superiores a 7.2.
    Remoção da função que exibe o nome do produto pois estava dando erro no script.

--- App/Database/carrinho.php
+++ App/Database/carrinho.php
@@ -20,7 +20,8 @@ if(isset($_POST['prodSubmit']) && $_POST['prodSubmit'] == "carrinho"){
 	}
 }	
 
-if(count($_SESSION['itens']) == 0){
+$pkCount = (is_array($_SESSION['itens']) ? count($_SESSION['itens']) : 0);
+  if ($pkCount == 0) {
 	echo ' Carrinho Vazios</br> ';
 
 }else{
@@ -28,24 +29,17 @@ if(count($_SESSION['itens']) == 0){
 	$vendas = new Vendas;
 	$cont = 1;
 	foreach ($_SESSION['itens'] as $produtos => $quantidade) {
-
-
-		$nomeProduto = $vendas->itemNome($produtos);
-
-		if( $nomeProduto != NULL){
+		
 			echo '<tr>
 			<td>'.$cont.'</td>
 			<td>'.$produtos.'</td>
-			<td>'. $nomeProduto .'</td>
 			<td>'.$quantidade.'</td>
 			<td><input type="hidden" id="idItem" name="idItem['.$produtos.']" value="'.$produtos.'" />
 			<input type="hidden" id="qtd" name="qtd['.$produtos.']" value="'.$quantidade.'" />
 			<a href="../../App/Database/remover.php?remover=carrinho&id='.$produtos.'"><i class="fa fa-trash text-danger"></i></a></td>
 			</tr>';	
 			$cont = $cont + 1;
-		}else{
-			unset($_SESSION['itens'][$produtos]);
-		}
+		
 	}
 }
 ?>
